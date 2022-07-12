7fa6aea413c407dbac210eb58d565e7e37a42f3c
Ivy Wong <ivywong@Ivys-MBP.attlocal.net>
2022-07-12 13:03:41 -0700

    Fizz Buzz Exercise

--- src/main/java/com/galvanize/Application.java
+++ src/main/java/com/galvanize/Application.java
@@ -3,12 +3,15 @@ package com.galvanize;
 public class Application {
 
     public static void main(String[] args) {
-        if (args.length == 2) {
-            System.out.println(args[0] + " <" + args[1] + ">");
-        } else if (args.length ==1) {
-            System.out.println(String.format("Please specify an email for %s", args[0]));
-        } else if (args.length == 0) {
-            System.out.println("Please specify a name and email");
+        int x = Integer.parseInt(args[0]);
+        if (x % 3 == 0 && x % 5 == 0) {
+            System.out.println("FizzBuzz");
+        } else if (x % 3 == 0) {
+            System.out.println("Fizz");
+        } else if (x % 5 == 0) {
+            System.out.println("Buzz");
+        } else {
+            System.out.printf(args[0]);
         }
     }
 
