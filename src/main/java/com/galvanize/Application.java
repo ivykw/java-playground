13c81cf0fba8cbafb3c49efc43bb040ecf57ed62
Ivy Wong <ivywong@Ivys-MBP.attlocal.net>
2022-07-12 10:59:12 -0700

    Java basics

--- src/main/java/com/galvanize/Application.java
+++ src/main/java/com/galvanize/Application.java
@@ -3,7 +3,13 @@ package com.galvanize;
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("hello world");
+        if (args.length == 2) {
+            System.out.println(args[0] + " <" + args[1] + ">");
+        } else if (args.length ==1) {
+            System.out.println(String.format("Please specify an email for %s", args[0]));
+        } else if (args.length == 0) {
+            System.out.println("Please specify a name and email");
+        }
     }
 
 }
